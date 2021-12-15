a7adfbb29281d7ac307301a70663a53efaa538d3
André Vega <ventaslederer@hotmail.com>
2021-12-15 17:44:12 -0600

    CSS: added font family, color variables and applied them to body and nav.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,6 +5,7 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = BASE_URL + '/search/movie?' + API_KEY;
 
 const section = document.getElementById('info')
+const resultsDiv = document.querySelector(".results__div")
 
 getMovies(API_URL)
 
@@ -16,8 +17,19 @@ function getMovies(url){
     })
 }
 
+function addCssClasses() {
+    
+    resultsDiv.innerHTML = 
+    `
+    <section id='info' class="results__section--movie-square p-2 bg-command-blue justify-items-center grid md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-8 gap-x-2">
+    </section>
+    `
+}
+
 function showMovies(data){
     info.innerHTML = '';
+
+
     data.forEach(movie => {
         const{title, poster_path, release_date, vote_average} = movie;
         const movieEl = document.createElement('div');
@@ -25,9 +37,9 @@ function showMovies(data){
         movieEl.innerHTML = `
         <img class="results__img--movie-poster" src="${IMG_URL + poster_path}" alt="${title}"> <!-- image poster -->
         <ul class="result__ul--information-list">
-        <li>${title}</li>
-        <li>${release_date}</li>
-        <li>${vote_average}</li>
+        <li class="text-2xl font-bold">${title}</li>
+        <li class="text-xl">${release_date}</li>
+        <li class="text-xl">${vote_average}</li>
         <!-- Basic information (Title, score, genre, year) -->
         </ul>
         `   
@@ -46,3 +58,7 @@ form.addEventListener('submit', (i)=>{
         getMovies(API_URL)
     }
 })
+
+// Call functions
+
+addCssClasses();
