ed8f244e81b982c5ea082f73826067730438c9b3
Leonardo Gonzalez <leogzz0@hotmail.com>
2022-01-05 15:24:22 -0600

    javascript functions for second html

--- assets/JS/movie_article.js
+++ assets/JS/movie_article.js
@@ -1,6 +1,5 @@
 const API_KEY = 'api_key=e158b050ab89151ee885d5873ddf8ffb';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = BASE_URL + '/search/movie?' + API_KEY;
 
@@ -9,5 +8,37 @@ const ID_URL = BASE_URL + '/movie/' + id + '?' + API_KEY;
 console.log(ID_URL)
 console.log(id)
 
-const section = document.getElementById('info')
+const section = document.getElementById('main')
 
+function getMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data)
+        console.log(data.results)
+        showMovies(data.results)
+    })
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.map(movie => {
+        const {title, poster_path, release_date, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.innerHTML = `
+        <div class="bg-command-blue flex p-6">
+            <img src="${IMG_URL + poster_path}" alt="Placeholder poster">
+            <div class="p-6"> 
+                <h2>${title}</h2>
+                <span>${vote_average} <span><h2>| Release date: ${release_date} | Sci-fi / Terror | 2h 10m</h2>
+                <p>${overview}</p>
+                <p>https://en.wikipedia.org/wiki/Annihilation_(film)</p>
+                <p>Directed by Steven Spielvergo</p>
+                <p>Cast: Chiclomino Rodríguez, Paco Pecas, Pitoloco Hernández</p>
+            </div>     
+        </div> 
+        `
+        main.appendChild(movieEl);
+    })
+}
+
+getMovies(ID_URL)
